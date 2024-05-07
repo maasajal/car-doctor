b2ce1165b611cfd729348134781b7032753e474c
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-07 09:06:46 +0300

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Header/Navbar";
 // Make the App as Layout component
 function App() {
   return (
     <div className="font-inter">
+      <Navbar />
       <Outlet />
     </div>
   );
